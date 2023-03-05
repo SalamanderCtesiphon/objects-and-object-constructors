005b6b9b7e32c868a4584e2019fe8f7b021c756c
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-05 14:52:53 -0600

    created a object constructor for creating book objects

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+function Book(title, author, numberOfPages, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.haveRead = false;
+  this.info = function() {
+    let readStatus = '';
+    haveRead ? readStatus = "have read" : readStatus = "not read yet";
+    return (`${title} by ${author}, ${numberOfPages} pages, ${readStatus}.`)
+  }
+}
+
+
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', true );
+
+const message = hobbit.info();
+
+const text = document.querySelector('.text');
+text.textContent = message;
