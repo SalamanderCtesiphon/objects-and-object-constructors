0c0f5f2449392ebb53484dea6276a7880ea8ed4e
salamanderctesiphon <swbrookshire@gmail.com>
2023-03-06 13:31:37 -0600

    styled the output and fixed an issue with the read status attribute

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ function Book(title, author, numberOfPages, haveRead) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
-  this.haveRead = false;
-  this.info = function() {
-    let readStatus = '';
-    haveRead ? readStatus = "have read" : readStatus = "not read yet";
-    return (`${title} by ${author}, ${numberOfPages} pages, ${readStatus}.`)
-  }
+  this.haveRead = haveRead;
 }
 
 const hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', true );
@@ -29,10 +24,19 @@ function addBookToLibrary() {
 function bookList() {
   const body = document.querySelector('.body');
   myLibrary.map((book) => {
+    let readStatus = '';
+    if (book.haveRead === true) {
+      readStatus = "Have read." 
+    } else {
+      readStatus = "Have not read yet."
+    };
     const bookDiv = document.createElement('div');
     bookDiv.classList.add('book');
     bookDiv.innerHTML = `
-      <h1>${book.title}</h1>
+      <h2>${book.title}</h2>
+      <p>Author: ${book.author}</p>
+      <p>Length: ${book.numberOfPages} pgs.</p>
+      <p>${readStatus}</p>
     `;
     body.appendChild(bookDiv);
   });
